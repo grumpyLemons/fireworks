1925f357d18c11ad2f207191be87f5e5f4b5e087
Mark <45207958+grumpyLemons@users.noreply.github.com>
2021-06-27 19:05:48 +0700

    some additions

--- graphics/bullet.h
+++ graphics/bullet.h
@@ -4,6 +4,17 @@ namespace Graphics{
     class Bullet : public Entity{
     public:
         void OnFrame();
+        void SetSplinterPos(int index, float x, float y)
+        {
+            if (splinters.size() > index){
+                splinters[index].x = x;
+                splinters[index].y = y;
+            }
+            else
+            {splinters.mplace_back(x, y);
+            }
+
+        };
         void SetExploded(bool value) {isExploded = value; };
         void SetBulletPosition(float x, float y) {
             currentX = x;
@@ -11,5 +22,11 @@ namespace Graphics{
     private:
         float currentX;
         float currentY;
+        struct Position final
+        {
+            float x;
+            float y;
+        };
+        std::vector<Position> splinters;
     };
 }
